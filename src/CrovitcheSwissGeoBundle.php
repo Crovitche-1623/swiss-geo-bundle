ab146470f3ea2bdbb4d1003bd3f10080770675f7
Thibault G <thibault@rolling.company>
2022-10-03 16:20:14 +0200

    fix(services): fix missing services configuration

--- src/CrovitcheSwissGeoBundle.php
+++ src/CrovitcheSwissGeoBundle.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Crovitche\SwissGeoBundle;
 
-use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
@@ -14,13 +17,15 @@ class CrovitcheSwissGeoBundle extends AbstractBundle
 {
     /**
      * {@inheritDoc}
+     *
+     * @throws \Exception
      */
-    public function configure(DefinitionConfigurator $definition): void
-    {
-        $definition
-            ->rootNode()
-
-            ->end()
-        ;
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $container,
+        ContainerBuilder $builder
+    ): void {
+        $loader = new YamlFileLoader($builder, new FileLocator(dirname(__DIR__).'/config'));
+        $loader->load('services.yaml');
     }
 }
