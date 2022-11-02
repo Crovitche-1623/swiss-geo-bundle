b65b1697e5526b9c92e563b2f5357e56e988cce3
Thibault G <thibault@rolling.company>
2022-11-03 00:42:35 +0100

    [assets] Added missing assets and package.json

---
+++ assets/controllers/tom-select_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select"
+
+export default class extends Controller {
+  // @see https://stimulus.hotwired.dev/reference/values
+  static values = {
+    url: String
+  }
+
+  connect() {
+    "use strict"
+
+    const parent = this
+
+    new TomSelect(this.element, {
+      valueField: 'egaid',
+      labelField: 'title',
+      // TODO: It's not possible to just get the data without searching ?????
+      searchField: ['title'],
+      create: false,
+      maxOptions: 7,
+      hideSelected: true,
+
+      // Minimum query length
+      shouldLoad: function (query) {
+        return query.length >= 2;
+      },
+
+      // Fetch remote data
+      load: function (query, callback) {
+        const url = parent.urlValue + '?q=' + encodeURIComponent(query)
+        fetch(url)
+          .then(response => response.json())
+          .then(json => {
+            callback(json.hits)
+          }).catch(() => {
+            callback()
+          })
+      },
+      plugins: ['dropdown_input'],
+    })
+  }
+}
