f3cb86a7c562474ba1a98d1b3b9956425333beac
Thibault G. <gattolliat.thibault@gmail.com>
2024-09-13 08:10:31 +0200

    Update CrovitcheSwissGeoBundle.php
    Signed-off-by: Thibault G. <gattolliat.thibault@gmail.com>

--- src/CrovitcheSwissGeoBundle.php
+++ src/CrovitcheSwissGeoBundle.php
@@ -17,8 +17,8 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 class CrovitcheSwissGeoBundle extends AbstractBundle
 {
     private const LOCALITIES_URL = 'https://data.geo.admin.ch/ch.swisstopo-vd.ortschaftenverzeichnis_plz/ortschaftenverzeichnis_plz/ortschaftenverzeichnis_plz_2056.csv.zip';
-    private const STREETS_URL = 'https://data.geo.admin.ch/ch.swisstopo.amtliches-strassenverzeichnis/csv/2056/ch.swisstopo.amtliches-strassenverzeichnis.zip';
-    private const BUILDING_ADDRESSES_URL = 'https://data.geo.admin.ch/ch.swisstopo.amtliches-gebaeudeadressverzeichnis/csv/2056/ch.swisstopo.amtliches-gebaeudeadressverzeichnis.zip';
+    private const STREETS_URL = 'https://data.geo.admin.ch/ch.swisstopo.amtliches-strassenverzeichnis/amtliches-strassenverzeichnis_ch/amtliches-strassenverzeichnis_ch_2056.csv.zip';
+    private const BUILDING_ADDRESSES_URL = 'https://data.geo.admin.ch/ch.swisstopo.amtliches-gebaeudeadressverzeichnis/amtliches-gebaeudeadressverzeichnis_ch/amtliches-gebaeudeadressverzeichnis_ch_2056.csv.zip';
     private const MEILISEARCH_URL = 'http://localhost:7071';
 
     public function configure(DefinitionConfigurator $definition): void
